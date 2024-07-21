694de35ae20d620da5097c079cb643175bd91009
jingwei.zhang <jingwei.zhang@bytedance.com>
2024-07-20 19:54:18 -0700

    fix flake8

--- src/service/audio_processing/transcribe_audio.py
+++ src/service/audio_processing/transcribe_audio.py
@@ -133,9 +133,15 @@ def transcribe_audio_zh(logger, path, modelName="base.en", language="en", srtFil
             subtitle.end = datetime.timedelta(seconds=word.end)
 
             # 排除英文字母+. 情况
-            if ((final_word[-1] in END_INTERPUNCTION and not (
-                    final_word[-1] == "." and len(final_word) > 1 and final_word[-2] in ENGLISH_AND_NUMBER_CHARACTERS)) or (len(subtitle.content) > 20)):
-                subtitle.content += final_word[:-1] if final_word[-1] == "." and final_word[-2] in ENGLISH_AND_NUMBER_CHARACTERS else final_word
+            if ((final_word[-1] in END_INTERPUNCTION and
+                 not (final_word[-1] == "." and len(final_word) > 1 and final_word[-2] in ENGLISH_AND_NUMBER_CHARACTERS)) or
+                    len(subtitle.content) > 20):
+
+                if final_word[-1] == "." and len(final_word) > 1 and final_word[-2] in ENGLISH_AND_NUMBER_CHARACTERS:
+                    subtitle.content += final_word[:-1]
+                else:
+                    subtitle.content += final_word
+
                 subs.append(subtitle)
                 index += 1
                 subtitle = None
