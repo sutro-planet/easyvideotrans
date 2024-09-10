7b21b7ca78061089f62ae55df1467b917e9e57a3
Fang Han <fhan0520@gmail.com>
2024-09-09 18:48:55 -0700

    Add metrics exports for celery task manager; Add soft timeout for celery tasks. change worker concurrency to 3.

--- src/task_manager/celery_tasks/__init__.py
+++ src/task_manager/celery_tasks/__init__.py
@@ -1,8 +1,10 @@
 import os
 import json
 import logging
+import threading
 
 from celery import Celery
+from prometheus_client import start_http_server
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
@@ -20,6 +22,9 @@ def load_celery_config():
     config['task_acks_late'] = os.getenv('CELERY_TASK_ACKS_LATE', str(config['task_acks_late']).lower()) in ['true',
                                                                                                              '1', 't',
                                                                                                              'y', 'yes']
+    config['task_time_limit'] = os.getenv('CELERY_TASK_TIME_LIMIT', config['task_time_limit'])
+    config['task_soft_time_limit'] = os.getenv('CELERY_TASK_SOFT_TIME_LIMIT', config['task_soft_time_limit'])
+    config['metrics_server_port'] = os.getenv('CELERY_METRICS_SERVER_PORT', config['metrics_server_port'])
     return config
 
 
@@ -35,3 +40,13 @@ logger.warning("Celery Configuration: %s", json.dumps(celery_config, indent=4))
 celery_app = Celery(__name__, include=["src.task_manager.celery_tasks.tasks"])
 celery_app.conf.update(load_celery_config())
 celery_app.conf.task_routes = load_tasks_route()
+
+
+# Start the metrics server in a separate thread
+def start_metrics_server():
+    metrics_port = celery_config['metrics_server_port']
+    logger.warning("Celery Metrics Server running on port: %s", metrics_port)
+    start_http_server(port=int(metrics_port))
+
+
+threading.Thread(target=start_metrics_server).start()
--- src/task_manager/celery_tasks/tasks.py
+++ src/task_manager/celery_tasks/tasks.py
@@ -1,9 +1,38 @@
+import time
+
 from src.service.video_synthesis.video_preview import zhVideoPreview
 from src.task_manager.celery_tasks import celery_app
+
+from prometheus_client import Counter, Histogram
+from celery.exceptions import SoftTimeLimitExceeded
+
+# Define Prometheus metrics
+VIDEO_PREVIEW_TASK_INVOKED = Counter(
+    'video_preview_task_invoked_total', 'Total number of times video preview task is invoked')
+VIDEO_PREVIEW_TASK_FAILED = Counter(
+    'video_preview_task_failed_total', 'Total number of times video preview task failed')
+VIDEO_PREVIEW_TASK_SOFT_TIMEOUT = Counter(
+    'video_preview_task_soft_timeout_total', 'Total number of times video preview task failed due to soft timeout')
+VIDEO_PREVIEW_TASK_DURATION = Histogram(
+    'video_preview_task_duration_seconds', 'Duration of video preview task in seconds')
 
 
 @celery_app.task(bind=True)
 def video_preview_task(self, video_path, voice_path, audio_bg_path, video_out_path):
     print(f"Invoke video preview task {self.request.id}.")
-    _ = zhVideoPreview(None, video_path, voice_path, audio_bg_path,
-                       "暂时没有处理字幕文件，所以随便写", video_out_path)
+    VIDEO_PREVIEW_TASK_INVOKED.inc()
+    start_time = time.time()
+
+    try:
+        _ = zhVideoPreview(None, video_path, voice_path, audio_bg_path,
+                           "暂时没有处理字幕文件，所以随便写", video_out_path)
+    except SoftTimeLimitExceeded as soft_exception:
+        VIDEO_PREVIEW_TASK_SOFT_TIMEOUT.inc()
+        print(f"Invoke video preview task {self.request.id} failed with soft timeout: {soft_exception}")
+    except Exception as exception:
+        VIDEO_PREVIEW_TASK_FAILED.inc()
+        print(f"Invoke video preview task {self.request.id} failed with exception: {exception}")
+    finally:
+        duration = time.time() - start_time
+        VIDEO_PREVIEW_TASK_DURATION.observe(duration)
+        print(f"Invoke video preview task {self.request.id} took {duration:.2f} seconds.")
